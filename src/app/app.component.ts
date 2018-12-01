d4b80f875b8dce5b8a878df4b54511813b3fccbf
Nagma22 <nagmashima7@gmail.com>
2018-12-01 13:02:33 +0000

    Commiting With Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App-ngClass-Directive';
+  cssStringVar: string= 'red size20';
+  cssClass: CssClass = new CssClass();
 }
+	
+ 
+class CssClass {
+    red: boolean= true;
+    size20: boolean= true;
+}
+ 
